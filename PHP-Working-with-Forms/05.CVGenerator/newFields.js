5ebf16659df88e304cf8425d6bbfb70a5e81475c
TzvetanIG <cecoig@mail.bg>
2014-08-18 21:20:01 +0300

    Add HTML validation

---
+++ PHP-Working-with-Forms/05.CVGenerator/newFields.js
@@ -0,0 +1,61 @@
+function addFieldProgrammingLang(ev) {
+    var button = ev.target;
+    var div = document.createElement('div');
+    div.innerHTML = '<input type="text" name="Computer Skills[Programming Languages][Language][]" pattern="[A-Za-zА-яа-я]{2,20}">' +
+        '<select name="Computer Skills[Programming Languages][Skill Level][]">' +
+        '<option value="beginner">Beginner</option>' +
+        '<option value="average">Average</option>' +
+        '<option value="excellent">Excellent</option>' +
+        '</select>' +
+        '<input type="button" name="remove" value="X">';
+
+    var programmingLang = document.getElementById('programmingLang');
+    programmingLang.appendChild(div);
+}
+
+var button1 = document.getElementById('AddProgramLang');
+button1.addEventListener('click', addFieldProgrammingLang);
+
+
+function addFieldLang(ev) {
+    var button = ev.target;
+    var div = document.createElement('div');
+    div.innerHTML = '<input type="text" name="Other Skills[Languages][Language][]" pattern="[A-Za-zА-яа-я]{2,20}">' +
+        '<select name="Other Skills[Languages][Comprehension][]">' +
+        '<option value="comprehension">-Comprehension-</option>' +
+        '<option value="beginner">Beginner</option>' +
+        '<option value="average">Average</option>' +
+        '<option value="excellent">Excellent</option>' +
+        '</select>' +
+        '<select name="Other Skills[Languages][Reading][]">' +
+        '<option value="reading">-Reading-</option>' +
+        '<option value="beginner">Beginner</option>' +
+        '<option value="average">Average</option>' +
+        '<option value="excellent">Excellent</option>' +
+        '</select>' +
+        '<select name="Other Skills[Languages][Writing][]">' +
+        '<option value="writing">-Writing-</option>' +
+        '<option value="beginner">Beginner</option>' +
+        '<option value="average">Average</option>' +
+        '<option value="excellent">Excellent</option>' +
+        '</select>' +
+        '<input type="button" name="remove" value="X">';
+
+    var programmingLang = document.getElementById('Lang');
+    programmingLang.appendChild(div);
+}
+
+var button2 = document.getElementById('AddLang');
+button2.addEventListener('click', addFieldLang);
+
+
+function removeField(ev){
+    var button = ev.target;
+    if(button.name == 'remove') {
+        var parent = button.parentNode;
+        var wrapper = parent.parentNode;
+        wrapper.removeChild(parent);
+    }
+}
+
+document.addEventListener('click', removeField);
